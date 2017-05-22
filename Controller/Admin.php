eb44b9442780eb96b8733ed130b9b258a9201a87
Walter Gamba <walter@iakta.it>
2017-05-22 23:06:00 +0200

    more logs for Hugo run

--- Controller/Admin.php
+++ Controller/Admin.php
@@ -343,6 +343,9 @@ class Admin extends \Cockpit\AuthController {
                 }
             }
             if($cmd_out || $error){
+                error_log("CMD OUT .$cmd_out.");
+                error_log("ERROR .$error.");
+
                 error_log("Returning error $cmd_out, $error".print_r($ret,1)." -- ".print_r($ret_lines,1 ));
                 $ret=array("status"=>"error","error"=>($error? $error : $ret));
 //                $this->app->response->status=500;
